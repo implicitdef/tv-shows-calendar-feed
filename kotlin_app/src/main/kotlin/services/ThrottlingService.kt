340e3d0a1d1695fc5fdf0eadd17743c416e345d3
implicitdef <implicitdef@users.noreply.github.com>
2018-04-01 21:30:32 +0200

    throttling working

--- kotlin_app/src/main/kotlin/services/ThrottlingService.kt
+++ kotlin_app/src/main/kotlin/services/ThrottlingService.kt
@@ -1,24 +1,26 @@
 package services
 
 import utils.CS
+import java.time.Duration
 import java.util.concurrent.DelayQueue
 import java.util.concurrent.Delayed
 import java.util.concurrent.TimeUnit
 
 object ThrottlingService {
 
-    // TODO doesn't work, reimplement with https://github.com/bbeck/token-bucket maybe
-
-
-    private const val maxCallsInWindow = 3
-    private val windowLengthSeconds = 1
+    private const val maxCallsInWindow = 40
+    private val windowLength = Duration.ofSeconds(10)
     // Initiate the queue with N tokens, with 0 delay so they can be immediately taken
-    private val initialTokens = (1..maxCallsInWindow).map {Token(0) }
+    private val initialTokens = (1..maxCallsInWindow).map { Token(Duration.ZERO) }
     private val registry = DelayQueue<Token>(initialTokens)
 
-    class Token(val delayInSeconds: Long): Delayed {
+    class Token(private val initialDelay: Duration) : Delayed {
+
+        private val availableAt = System.currentTimeMillis() + initialDelay.toMillis()
+
         override fun getDelay(unit: TimeUnit): Long =
-           unit.convert(delayInSeconds, TimeUnit.SECONDS)
+            availableAt - System.currentTimeMillis()
+
         override fun compareTo(other: Delayed): Int {
             val a = getDelay(TimeUnit.NANOSECONDS)
             val b = other.getDelay(TimeUnit.NANOSECONDS)
@@ -29,14 +31,14 @@ object ThrottlingService {
     }
 
     fun <Output> withThrottling(func: () -> CS<Output>): CS<Output> {
+        //log("Number of tokens available: ${registry.size}")
         // take a token
         // will block if there's no token available
-        registry.take()
+        val token = registry.take()
         return func().whenComplete { _, _ ->
             // Puts a token back, but it will be available only in some time
-            registry.put(Token(windowLengthSeconds.toLong()))
+            val newToken = Token(windowLength)
+            registry.put(newToken)
         }
     }
-
-
 }
