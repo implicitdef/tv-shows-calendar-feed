c68b16e7ff87c71ee12f1c47516f44c194ba70a0
implicitdef <implicitdef@users.noreply.github.com>
2018-04-07 11:46:08 +0200

    Forget the server and heroku, back to a manual script

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,12 @@
 import services.FetchingService
 import tvshowscalendar.TvShowsCalendarClient
 import utils.CS
-import utils.HttpServer
-import utils.Scheduler
 import utils.Utils.log
 import utils.Utils.threadPool
 import java.util.concurrent.TimeUnit
 
-/*
-TODO monitor execution on heroku
-*/
-
 fun main(args: Array<String>) {
     log("Starting the app")
-    Scheduler.schedule {
-        log("Starting the fetch/push")
-        FetchingService.fetch().thenCompose { seriesWithSeasons ->
-            TvShowsCalendarClient.pushData(TvShowsCalendarClient.Target.HEROKU, seriesWithSeasons)
-        }.logIfException()
-    }
-    HttpServer.start()
-}
-
-fun fetchAllAndSendToHerokuNodAndLogAndKillThreadPool() {
     FetchingService.fetch().thenCompose { seriesWithSeasons ->
         TvShowsCalendarClient.pushData(TvShowsCalendarClient.Target.HEROKU, seriesWithSeasons)
     }
@@ -38,7 +22,7 @@ fun <T> CS<T>.logIfException(): CS<T> =
     }
 
 fun <T> CS<T>.shutdownThreadPoolAfter(): CS<T> =
-    this.whenComplete { _, exception ->
+    this.whenComplete { _, _ ->
         shutdown()
     }
 
