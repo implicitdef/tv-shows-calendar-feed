70bd8b47a7376e7c03572043480488a7c7c560ac
implicitdef <implicitdef@users.noreply.github.com>
2020-05-01 13:17:34 +0200

    fetching works

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { fetchAll, fetchForPage } from './fetchingService'
 
 async function start() {
   try {
-    const mm = await fetchAll(3)
+    const mm = await fetchAll()
     console.log('OK', mm)
   } catch (err) {
     console.log(err)
