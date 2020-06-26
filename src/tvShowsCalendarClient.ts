91513c27e1d2763303c23961a9cf1fa0d30c30d0
implicitdef <implicitdef@users.noreply.github.com>
2020-06-26 12:39:30 +0200

    fix url and prettier

--- src/tvShowsCalendarClient.ts
+++ src/tvShowsCalendarClient.ts
@@ -1,31 +1,31 @@
-import { FullSerie } from "./myTypes.ts";
-import { reverse } from "./utils.ts";
+import { FullSerie } from './myTypes.ts'
+import { reverse } from './utils.ts'
 
 const targets = {
   local: {
-    url: "http://localhost:3333",
-    apiKey: "pushDataApiKey",
+    url: 'http://localhost:3333',
+    apiKey: 'pushDataApiKey',
   },
   heroku: {
-    url: "https://tv-shows-calendar.herokuapp.com",
-    apiKey: reverse("zL84m>:Y"),
+    url: 'https://tv-shows-calendar.herokuapp.com',
+    apiKey: reverse('zL84m>:Y'),
   },
-};
+}
 
 // La structure de donnée attendue par le serveur
 type ExpectedDataFormat = {
   serie: {
-    id: number;
-    name: string;
-  };
+    id: number
+    name: string
+  }
   seasons: {
-    number: number;
+    number: number
     time: {
-      start: string;
-      end: string;
-    };
-  }[];
-}[];
+      start: string
+      end: string
+    }
+  }[]
+}[]
 
 function restructureData(input: FullSerie[]): ExpectedDataFormat {
   return input.map(({ id, name, seasons }) => ({
@@ -37,25 +37,25 @@ function restructureData(input: FullSerie[]): ExpectedDataFormat {
         end: end.toISOString(),
       },
     })),
-  }));
+  }))
 }
 
 export async function pushData(
   target: keyof typeof targets,
   data: FullSerie[],
 ) {
-  const { url, apiKey } = targets[target];
-  console.log(">> Posting data to ", url);
-  const url2 = new URL(`${url}/data`);
-  url2.searchParams.append("key", apiKey);
-  const res = await fetch(`${url}/data`, {
-    method: "POST",
+  const { url, apiKey } = targets[target]
+  console.log('>> Posting data to ', url)
+  const url2 = new URL(`${url}/data`)
+  url2.searchParams.append('key', apiKey)
+  const res = await fetch(url2, {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(restructureData(data)),
-  });
+  })
   if (!res.ok) {
-    throw new Error(`HTTP response was not OK ${res.status} ${url}`);
+    throw new Error(`HTTP response was not OK ${res.status} ${url}`)
   }
 }
